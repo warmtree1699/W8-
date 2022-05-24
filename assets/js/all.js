b7241730cfd8c6b755989072bd2e8285b78f6001
Andy <warmtree1699@gmail.com>
2022-05-24 17:49:40 +0800

    Update 2022-05-24T09:49:36.616Z

---
+++ assets/js/all.js
@@ -0,0 +1,16 @@
+"use strict";
+
+$(function () {
+  console.log('Hello Bootstrap5');
+});
+"use strict";
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  }
+});
+//# sourceMappingURL=all.js.map
